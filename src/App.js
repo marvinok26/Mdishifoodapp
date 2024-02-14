ed0c0706856e0cd73d9b5d725815dd8f9c45a9dd
Marvin <okongomarvin971@gmail.com>
2024-02-14 13:20:16 +0300

    App.js Amend1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-
+import Footer from './Footer'
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 };
